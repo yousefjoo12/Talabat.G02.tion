a879a12bc7bd849ce15737809b1e73fe0d62a0f1
Youssef <yousefxbxd12@gmail.com>
2024-10-02 10:53:15 +0300

    ProductsController

---
+++ Talabat.Repository/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entites;
+using Talabat.Core.Reposeitories.Contract;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbcontext;
+
+        public GenericRepository(StoreContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+           return await _dbcontext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await _dbcontext.Set<T>().FindAsync(id);
+        }
+    }
+}
