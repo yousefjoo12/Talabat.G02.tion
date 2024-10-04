95b988a930e1d0a640f5415acd1396a013a34d6b
Youssef <yousefxbxd12@gmail.com>
2024-10-04 12:59:19 +0300

    update

--- Talabat.APIS/Controllers/ProductsController.cs
+++ Talabat.APIS/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Talabat.Core.Entites;
 using Talabat.Core.Reposeitories.Contract;
+using Talabat.Core.Specifications.ProductSpecifications;
 
 namespace Talabat.APIS.Controllers
 {
@@ -18,14 +19,16 @@ namespace Talabat.APIS.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProductsAsync()
         {
-            var products = await _repository.GetAllAsync();
+            var spec = new ProductWithBrandAndCategotySpecifications();
+            var products = await _repository.GetAllWithSpecAsync(spec);
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _repository.GetAsync(id);
+            var spec = new ProductWithBrandAndCategotySpecifications(id);
+            var product = await _repository.GetWithspecAsync(spec);
             if (product == null)
             {
                 return NotFound(new { Message = "NotFound", StatusCode = 404 });
---
+++ Talabat.Core/Specifications/ProductSpecifications/ProductWithBrandAndCategotySpecifications.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entites;
+
+namespace Talabat.Core.Specifications.ProductSpecifications
+{
+    public class ProductWithBrandAndCategotySpecifications:BaseSpecifications<Product>
+    {
+        public ProductWithBrandAndCategotySpecifications():base()
+        {
+            Includes.Add(p=>p.Brand);
+            Includes.Add(p=>p.Category);
+        }
+        public ProductWithBrandAndCategotySpecifications(int id):base(P=>P.Id==id)
+        {
+            Includes.Add(p => p.Brand);
+            Includes.Add(p => p.Category);
+        }
+    }
+}
